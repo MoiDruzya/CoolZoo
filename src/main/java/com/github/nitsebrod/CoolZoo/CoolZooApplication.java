43bd04f5376ae8d7ac3633f8191a30f6bd68d243
Nitsebrod <tuter2000@icloud.com>
2024-04-09 19:38:55 +0300

    Gradle init commit

---
+++ src/main/java/com/github/nitsebrod/CoolZoo/CoolZooApplication.java
@@ -0,0 +1,13 @@
+package com.github.nitsebrod.CoolZoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolZooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolZooApplication.class, args);
+	}
+
+}
